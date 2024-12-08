66a9b0b9b723deea3868290a505c69b4e2c89004
이재영 <ldc1000@naver.com>
2024-12-08 15:03:27 +0900

    ✨NavigationController presnet EditorViewController

--- NBC_PhoneBook/NBC_PhoneBook/source/ViewControllers/MainViewController.swift
+++ NBC_PhoneBook/NBC_PhoneBook/source/ViewControllers/MainViewController.swift
@@ -11,15 +11,45 @@ import SnapKit
 
 /// 메인 화면 ViewController
 final class MainViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("1")
-        self.view.backgroundColor = .red
+        
+        self.view.backgroundColor = .white
+        configureNavigationController()
     }
-
+    
 }
 
-#Preview {
-    MainViewController()
+//MARK: - NavigationController
+extension MainViewController {
+    
+    //네비게이션 컨트롤러 설정 메서드
+    private func configureNavigationController() {
+        self.navigationItem.title = "친구 목록"
+        self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 20, weight: .bold)]
+        
+        let addButton = UIBarButtonItem(title: "추가",
+                                        style: .plain,
+                                        target: self,
+                                        action: #selector(pushEditerView))
+        
+        self.navigationItem.setRightBarButton(addButton, animated: false)
+    }
+    
+    @objc private func pushEditerView() {
+        guard let navigationController = self.navigationController else { return }
+        navigationController.pushViewController(EditorViewController(), animated: false)
+    }
+    
 }
+
+//#Preview {
+//    MainViewController()
+//}
