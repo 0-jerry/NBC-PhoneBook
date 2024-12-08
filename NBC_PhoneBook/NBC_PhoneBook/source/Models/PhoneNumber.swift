7c4161998cc468307b27c0594971737fa1d7ad38
이재영 <ldc1000@naver.com>
2024-12-08 17:55:26 +0900

    ✨ PhoneNumber

--- NBC_PhoneBook/NBC_PhoneBook/source/Models/PhoneNumber.swift
+++ NBC_PhoneBook/NBC_PhoneBook/source/Models/PhoneNumber.swift
@@ -9,4 +9,35 @@ import Foundation
 
 struct PhoneNumber {
     
+    let pokeNumber: Int
+    
+    let name: String
+    
+    let phoneNumber: String
+    
+    static let mockDatas: [PhoneNumber] = [PhoneNumber(pokeNumber: 1,
+                                                       name: "이재영",
+                                                       phoneNumber: "010-1234-1234"),
+                                           PhoneNumber(pokeNumber: 2,
+                                                       name: "이재영이재영",
+                                                       phoneNumber: "010-0000-0000"),
+                                           PhoneNumber(pokeNumber: 3,
+                                                       name: "이재영이재영이재영이재영",
+                                                       phoneNumber: "010-1234-1234"),
+                                           PhoneNumber(pokeNumber: 4,
+                                                       name: "이재영이재영",
+                                                       phoneNumber: "010-0000-0000"),
+                                           PhoneNumber(pokeNumber: 5,
+                                                       name: "jae",
+                                                       phoneNumber: "010-1234-1234"),
+                                           PhoneNumber(pokeNumber: 6,
+                                                       name: "young",
+                                                       phoneNumber: "010-0000-0000"),
+                                           PhoneNumber(pokeNumber: 7,
+                                                       name: "jae",
+                                                       phoneNumber: "010-1234-1234"),
+                                           PhoneNumber(pokeNumber: 8,
+                                                       name: "young",
+                                                       phoneNumber: "010-0000-0000")]
+    
 }
